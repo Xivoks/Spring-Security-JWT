981cff9031bace4674295f30a6b0502600a33d51
Janek <j.brunow@lastlevel.pl>
2023-03-16 11:24:08 +0100

    adding files related with jwt filter.

--- src/main/java/com/workshop/security/config/SecurityConfig.java
+++ src/main/java/com/workshop/security/config/SecurityConfig.java
@@ -1,11 +1,39 @@
 package com.workshop.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.Filter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAthFilter jwtAuthFilter;
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User(
+                    "user@gmail.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
+            ),
+            new User(
+                    "user1@gmail.com",
+                    "password",
+                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
+            )
+    );
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -13,7 +41,16 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> APPLICATION_USERS
+                .stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
     }
 }
