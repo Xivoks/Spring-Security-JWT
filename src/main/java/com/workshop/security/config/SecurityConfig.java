5ced1cf9db6bbf7222c07663be19b4f6a3395248
Janek <j.brunow@lastlevel.pl>
2023-03-17 14:06:03 +0100

    changes related with AuthenticationController

--- src/main/java/com/workshop/security/config/SecurityConfig.java
+++ src/main/java/com/workshop/security/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.workshop.security.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,7 +14,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -33,6 +34,11 @@ public class SecurityConfig {
                     Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
             )
     );
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
